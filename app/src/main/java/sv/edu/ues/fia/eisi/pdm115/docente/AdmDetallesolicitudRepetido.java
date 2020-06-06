ffb3cbde41cdaee36ee197d0bbb796ea40aada97
CS17049 <joseamilcar19960011@gmail.com>
2020-06-06 08:04:48 -1100

    Repetidos

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmDetallesolicitudRepetido.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmDetallesolicitudRepetido.java
@@ -2,15 +2,26 @@ package sv.edu.ues.fia.eisi.pdm115.docente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import sv.edu.ues.fia.eisi.pdm115.R;
 
 public class AdmDetallesolicitudRepetido extends AppCompatActivity {
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adm_detallesolicitud_repetido);
+        boton = (Button) findViewById(R.id.irAprobarSolicitudRepetido);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdmDetallesolicitudRepetido.this, AdmAprobarSolRepetido.class));
+            }
+        });
     }
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmRepetidoActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmRepetidoActivity.java
@@ -2,15 +2,50 @@ package sv.edu.ues.fia.eisi.pdm115.docente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import sv.edu.ues.fia.eisi.pdm115.ControlBdGrupo12;
 import sv.edu.ues.fia.eisi.pdm115.R;
 
-public class AdmRepetidoActivity extends AppCompatActivity {
+public class AdmRepetidoActivity extends ListActivity {
+
+    String[] menu={"Solicitud 1 "};
+    String[] activities={"AdmDetallesolicitudRepetido"};
+    ControlBdGrupo12 BDhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adm_repetido2);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, menu));
+        // BDhelper = new ControlBDGrupo12(this);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if(position!=5){
+
+            String nombreValue=activities[position];
+
+            try{
+                Class<?>
+                        clase=Class.forName("sv.edu.ues.fia.eisi.pdm115.docente."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }else{
+          /*  BDhelper.abrir();
+            String tost=BDhelper.llenarBDCarnet();
+            BDhelper.cerrar(); */
+            Toast.makeText(this, "falta implementacion", Toast.LENGTH_SHORT).show();
+        }
     }
 }
