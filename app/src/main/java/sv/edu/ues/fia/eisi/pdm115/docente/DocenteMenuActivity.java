715434ff46745338ba35649796e27b2bdefcb4ac
CS17049 <joseamilcar19960011@gmail.com>
2020-06-06 00:46:27 -1100

    Paquetes

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/DocenteMenuActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.eisi.pdm115.docente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocenteMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_docente_menu);
+    }
+}
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/encargadoImpresion/EncargadoImpresionesMenuActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.eisi.pdm115.encargadoImpresion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncargadoImpresionesMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encargado_impresiones_menu);
+    }
+}
