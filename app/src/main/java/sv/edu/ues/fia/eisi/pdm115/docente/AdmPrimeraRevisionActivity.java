c3333d058212477b7525a5fbeaea2a3f45a869dc
CS17049 <joseamilcar19960011@gmail.com>
2020-06-08 06:29:06 -1100

    Modificaciones al COntrolBD

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmPrimeraRevisionActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmPrimeraRevisionActivity.java
@@ -31,8 +31,6 @@ public class AdmPrimeraRevisionActivity extends ListActivity {
     String [] opciones;
     ControlBdGrupo12 helper;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
