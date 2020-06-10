e243b68fb69849dc4aa0a925d0a404c6b4c386af
CS17049 <joseamilcar19960011@gmail.com>
2020-06-09 23:16:16 -1100

    Aprobar Diferido 1.5

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmRepetidoActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/docente/AdmRepetidoActivity.java
@@ -27,6 +27,5 @@ public class AdmRepetidoActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_lista);
-
     }
 }
