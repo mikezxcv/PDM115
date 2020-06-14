6d09c7848885bad51bcba20e43ceb07aa929358e
YOVANIHA <yovaniha96@gmail.com>
2020-06-14 01:52:03 -0600

    REPETIDO: SOLICITUD Y CONSULTA

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/estudiante/EstudianteConsultarSolicitudRepetidoActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.eisi.pdm115.estudiante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstudianteConsultarSolicitudRepetidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estudiante_consultar_solicitud_repetido);
+    }
+}
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/estudiante/EstudianteSolicitudRepetidoActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.eisi.pdm115.estudiante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstudianteSolicitudRepetidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estudiante_solicitud_repetido);
+    }
+}
